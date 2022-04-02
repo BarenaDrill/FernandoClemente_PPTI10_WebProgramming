1de8a639004e9d76d27b3427e9ccdec9ae294199
Fernando Clemente <fernandoclemente73@gmail.com>
2022-04-02 12:42:15 +0800

    connect to database and verify data from database

--- Self_Learning_1/loginProses.php
+++ Self_Learning_1/loginProses.php
@@ -1,14 +1,33 @@
 <?php
     session_start();
+    
+    $db = mysqli_connect("localhost","root","","selfLearning2");
+   
    // var_dump($_POST['submit']) ;
     if( isset($_POST['submit']) ){
-        if(($_POST['username'] ==  $_SESSION['usernameSession']) && ($_POST['password'] == $_SESSION['passwordSession'])) {
-            header("location:home.php");
+        $username = $_POST['username'] ;
+        $password = $_POST['password'] ;
+        
+        $result = mysqli_query($db,"SELECT * FROM user WHERE username = '$username' ") ;
+
+        if(mysqli_num_rows($result) === 1 ){
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($password,$row['password'])){
+                $_SESSION['id'] = $row['id'] ;
+                header("Location: home.php");
+                exit;
+            }else{
+                echo "<script>
+                  alert('Maaf anda gagal login pastikan username dan password sesuai');
+                  document.location.href = 'login.php' ;
+               </script>" ;
+            }
         }else{
             echo "<script>
-                  alert('maaf anda gagal login pastikan username dan password sesuai');
+                  alert('Username anda belum terdaftar');
                   document.location.href = 'login.php' ;
                </script>" ;
         }
+
     }
 ?>
